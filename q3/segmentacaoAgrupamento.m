b5641250280179ba73d0dcad8e51a5751dab81cb
PauloRRR <ricardo.paulorr@gmail.com>
2019-07-06 23:09:09 -0300

    adicionada segmentação por agrupamento

---
+++ q3/segmentacaoAgrupamento.m
@@ -0,0 +1,43 @@
+he = imread('coins.jpg');
+figure,imshow(he),title('imagem de patologia');
+cform = makecform('srgb2lab');
+lab_he = applycform(he,cform);
+ab = double(lab_he(:,:,2:3));
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+nColors = 3;
+[cluster_idx,cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+figure,imshow(pixel_labels,[]),title('imagem rotulada pelo indice do agrupamento');
+segmented_images = cell(1,3);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+for k=1:nColors
+    color = he;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+    txt = ['objetos no agrupamento ', num2str(k)];
+    figure,imshow(segmented_images{k}),title(txt);
+end
+
+he = imread('ballpool.jpg');
+figure,imshow(he),title('imagem de patologia');
+cform = makecform('srgb2lab');
+lab_he = applycform(he,cform);
+ab = double(lab_he(:,:,2:3));
+nrows = size(ab,1);
+ncols = size(ab,2);
+ab = reshape(ab,nrows*ncols,2);
+nColors = 5;
+[cluster_idx,cluster_center] = kmeans(ab,nColors,'distance','sqEuclidean','Replicates',3);
+pixel_labels = reshape(cluster_idx,nrows,ncols);
+figure,imshow(pixel_labels,[]),title('imagem rotulada pelo indice do agrupamento');
+segmented_images = cell(1,3);
+rgb_label = repmat(pixel_labels,[1 1 3]);
+for k=1:nColors
+    color = he;
+    color(rgb_label ~= k) = 0;
+    segmented_images{k} = color;
+    txt = ['objetos no agrupamento ', num2str(k)];
+    figure,imshow(segmented_images{k}),title(txt);
+end
